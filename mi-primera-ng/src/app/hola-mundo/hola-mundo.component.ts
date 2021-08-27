73a563b59e7205bcebc8fa1f325a68a5e60f5bfa
Luz De Leon <aracely.des@uanl.edu.mx>
2021-08-26 20:13:25 -0500

    ngif

--- mi-primera-ng/src/app/hola-mundo/hola-mundo.component.ts
+++ mi-primera-ng/src/app/hola-mundo/hola-mundo.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core'; // imports los miembros que necesitamos
 
-@Component({ //decorador del component
-  selector: 'app-hola-mundo', //metadatos y template
-  templateUrl: './hola-mundo.component.html',
+@Component({ //decorador del component //metadatos y template
+  selector: 'app-hola-mundo', //define el nombre la directiva, un simple tag de HTML
+  templateUrl: './hola-mundo.component.html', //el HTML que renderiza el navegador
   styleUrls: ['./hola-mundo.component.scss']
 })
 export class HolaMundoComponent implements OnInit { //propiedades de la clase y metodos necesitados por la vista
 
   mensaje: string = 'Hola, mundo!';
+  testBoolean : boolean = true;
+  numerosArreglo: Array<string> =[
+    '1', 
+    '2',
+  ] 
+
+  triggerClick(event: any) {
+    alert("test test test")
+  }
 
   constructor() { }
 
@@ -15,3 +24,4 @@ export class HolaMundoComponent implements OnInit { //propiedades de la clase y 
   }
 
 }
+
